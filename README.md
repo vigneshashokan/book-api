dd46fcb260dcdb4c591dc1f29f1903be7843cf9f
Vignesh Ashokan <vigneshgashokan@gmail.com>
2020-05-03 15:27:50 -0600

    diesel integration for postgres data store

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # book-api
 A sample API for books on rust-lang
+
+## Database setup locally with Diesel
+1. Set up the database with
+`$ diesel setup`
+2. Generate `up.sql` and `down.sql` by
+`$ diesel migration generate create_books`
+3. Edit the `up.sql` and `down.sql` files with DMLs and run the migration 
+`$ diesel migration run`
+4. It is good practice to run rollback immediately to check rollback
+`$ diesel migration redo`
+5. Add table macro to `schema.rs`
+`$ diesel print-schema > src/schema.rs`
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,30 @@ extern crate diesel;
 // extern crate diesel_codegen;
 extern crate dotenv;
 
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
 use dotenv::dotenv;
 use std::env;
 
-mod schema;
 mod models;
+mod schema;
 
 fn main() {
-    println!("Hello, world!");
+    // Check if the .env file is present
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("set DATABASE_URL");
+    let conn = PgConnection::establish(&database_url).unwrap();
+
+    let book = models::NewBook {
+        title: String::from("Gravity's Rainbow"),
+        author: String::from("Thomas Pynchon"),
+        published: true,
+    };
+
+    if models::Book::insert(book, &conn) {
+        println!("success!");
+    } else {
+        println!("failed");
+    }
 }
