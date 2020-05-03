cb56f65f669f7a95fb3152329af55df5706cc98d
Vignesh Ashokan <vigneshgashokan@gmail.com>
2020-05-03 09:21:04 -0600

    initial commit

---
+++ src/models.rs
@@ -0,0 +1,11 @@
+use diesel;
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+
+#[derive(Queryable)]
+pub struct Book {
+    pub id: i32,
+    pub title: String,
+    pub author: String,
+    pub ublished: bool,
+}
